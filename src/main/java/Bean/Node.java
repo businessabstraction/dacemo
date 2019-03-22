b8a5f6312ba12be4356be209dc1ac39dd9ec6552
Wenrui Li <u6361099@anu.edu.au>
2019-03-22 17:43:30 +1100

    merge request

---
+++ src/main/java/Bean/Node.java
@@ -0,0 +1,16 @@
+package Bean;
+
+public class Node {
+    private String id;
+    private int group;
+
+    public Node(String id, int group){
+        this.id = id;
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + ", group: " + group;
+    }
+}

90735a71855498404f3a3bba11f4e6f3db2bcfe4
Wenrui Li <u6361099@anu.edu.au>
2019-03-23 13:36:44 +1100

    Add attributes to the node class

--- src/main/java/Bean/Node.java
+++ src/main/java/Bean/Node.java
@@ -3,15 +3,23 @@ package Bean;
 public class Node {
     private String id;
     private int group;
+    private String label = "default";
+    private int level = 0;
 
     public Node(String id, int group){
         this.id = id;
         this.group = group;
     }
 
+    public Node(String id, int group,String label,int level){
+        this.id = id;
+        this.group = group;
+        this.label = label;
+        this.level = level;
+    }
 
     @Override
     public String toString() {
-        return "" + id +","+group;
+        return "" + id +","+group +","+label +","+level;
     }
 }

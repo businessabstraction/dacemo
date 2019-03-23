43922ef0f22eebdce1a55238d2e15b727b6b8d91
Tommy Gatti <tommy.gatti@outlook.com>
2019-03-24 00:19:38 +1100

    Added basic labelling function so D3 nodes don't just show the IRIs.

--- src/main/java/Bean/Node.java
+++ src/main/java/Bean/Node.java
@@ -3,12 +3,15 @@ package Bean;
 public class Node {
     private String id;
     private int group;
-    private String label = "default";
+    private String label;
     private int level = 0;
 
     public Node(String id, int group){
         this.id = id;
         this.group = group;
+        this.label = id.lastIndexOf("/") == -1 ?
+                id.substring(id.lastIndexOf("/") + 1) :
+                id; // TODO: 23/03/2019 Basic Labelling function, update.
     }
 
     public Node(String id, int group,String label,int level){

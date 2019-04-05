f57380974cdf46f2f75386a2a5f33b6bc1b9cf04
Lifu Zhao <lifu.zhao@anu.edu.au>
2019-04-05 18:46:29 +0800

    Adding nodes expanding servlet

---
+++ src/main/java/Servlet/NodeExpandServlet.java
@@ -0,0 +1,46 @@
+package Servlet;
+
+import Bean.D3Object;
+import Bean.Node;
+import database.StardogTriplesDBConnection;
+import database.format.GenericValue;
+import database.format.SPARQLResultTable;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "NodeExpandServlet",urlPatterns = "NodeExpand")
+public class NodeExpandServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nodename = request.getParameter("");
+        StardogTriplesDBConnection connection = new StardogTriplesDBConnection("iteration0", "http://localhost:5820", "admin", "admin");
+        if (connection.canConnect()){
+            SPARQLResultTable result = connection.selectQuery(
+                    "SPARQL"
+            );
+            /**
+             * Creatd the node arraylist and created the node object based on the SPARQL reqults
+             */
+            ArrayList<Node> nodes = new ArrayList<>();
+            for (GenericValue value : result.getValuesOfAttribute("s")){
+                Node node = new Node(value.get(), 1);
+                nodes.add(node);
+            }
+            D3Object d3Object = new D3Object(nodes);    // Translate result into JSON format
+
+            //TODO currently I use the String to transfer the data to the frontend. More Json things need to be done.
+            String stringFormat = d3Object.toString();
+            response.getOutputStream().print(stringFormat);
+        }
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

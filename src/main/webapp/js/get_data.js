a82dac0e5ddf5b0184d40758f8e3cc74452b9ced
Tommy Gatti <tommy.gatti@outlook.com>
2019-04-07 02:33:59 +1100

    updated get_data.js: added code fixes and rearranged arrow heads.

--- src/main/webapp/js/get_data.js
+++ src/main/webapp/js/get_data.js
@@ -1,23 +1,23 @@
 let getJson;
-var linkss;
+let linkss;
 
 
 /*=========================parse node===========================*/
 //get the node from json file
 function updateNode() {
 
-    var jsonObjects = JSON.parse(getJson);
-    console.log(jsonObjects[0][0].label)
+    const jsonObjects = JSON.parse(getJson);
+    console.log(jsonObjects[0][0].label);
     linkss = new Array(jsonObjects[1].length);
 
     //parse the json to array
-    for(var i =0; i<jsonObjects[1].length;i++){
+    for(let i =0; i<jsonObjects[1].length; i++){
 
-        linkss[i] = new Object();
-        linkss[i].target = jsonObjects[0][0].label;
-        linkss[i].targerid = jsonObjects[0][0].id;
-        linkss[i].source = jsonObjects[2][i].label;
-        linkss[i].sourceid = jsonObjects[2][i].id;
+        linkss[i] = {};
+        linkss[i].target = jsonObjects[2][i].label;
+        linkss[i].targerid = jsonObjects[2][i].id;
+        linkss[i].source = jsonObjects[0][0].label;
+        linkss[i].sourceid = jsonObjects[0][0].id;
         linkss[i].rela = jsonObjects[1][i].label;
         linkss[i].relaid = jsonObjects[1][i].id;
         linkss[i].type = "resolved";
@@ -80,45 +80,45 @@ function sendRequenst(node) {
 /*
 *  just for testing
 * */
-var linksss = [
-    { target: "mammal", source: "dog" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "mammal", source: "cat" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "mammal", source: "fox" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "mammal", source: "elk" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "insect", source: "ant" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "insect", source: "bee" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "fish"  , source: "carp", strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "fish"  , source: "pike", strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "cat"   , source: "elk" , strength: 0.1, rela:"xxx" ,type: "resolved"},
-    { target: "carp"  , source: "ant" , strength: 0.1, rela:"xxx" ,type: "resolved"},
-    { target: "elk"   , source: "bee" , strength: 0.1, rela:"xxx" ,type: "resolved"},
-    { target: "dog"   , source: "cat" , strength: 0.1, rela:"xxx" ,type: "resolved"},
-    { target: "fox"   , source: "ant" , strength: 0.1, rela:"xxx" ,type: "resolved"},
-    { target: "pike"  , source: "cat" , strength: 0.1, rela:"xxx" ,type: "resolved"}
-]
+const linksss = [
+    {target: "mammal", source: "dog", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "mammal", source: "cat", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "mammal", source: "fox", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "mammal", source: "elk", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "insect", source: "ant", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "insect", source: "bee", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "fish", source: "carp", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "fish", source: "pike", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "cat", source: "elk", strength: 0.1, rela: "xxx", type: "resolved"},
+    {target: "carp", source: "ant", strength: 0.1, rela: "xxx", type: "resolved"},
+    {target: "elk", source: "bee", strength: 0.1, rela: "xxx", type: "resolved"},
+    {target: "dog", source: "cat", strength: 0.1, rela: "xxx", type: "resolved"},
+    {target: "fox", source: "ant", strength: 0.1, rela: "xxx", type: "resolved"},
+    {target: "pike", source: "cat", strength: 0.1, rela: "xxx", type: "resolved"}
+];
 
 
-var extralinks = [
-    { target: "mammal", source: "dog" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "mammal", source: "cat" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "mammal", source: "fox" , strength: 0.7, rela:"xxx" ,type: "resolved"},
-    { target: "mammal", source: "elk" , strength: 0.7, rela:"xxx" ,type: "resolved"}
+const extralinks = [
+    {target: "mammal", source: "dog", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "mammal", source: "cat", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "mammal", source: "fox", strength: 0.7, rela: "xxx", type: "resolved"},
+    {target: "mammal", source: "elk", strength: 0.7, rela: "xxx", type: "resolved"}
 
-]
+];
 
 
 /*===========================parameters for currant node displaying==============================*/
 //store the links
-var links = [];
+const links = [];
 //store the nodes
-var nodes = {};
+let nodes = {};
 
 
 /*====================================drawing graph========================================*/
 //parse the node and link into node and links
 function processLink(linkss) {
 
-    for (var i = 0; i < linkss.length; i++) {
+    for (let i = 0; i < linkss.length; i++) {
         links[i] = {
             source: linkss[i].source,
             target: linkss[i].target,
@@ -141,14 +141,15 @@ function buildGraph(graphics,graphicsid,linkss)
 
     processLink(linkss);
 
-    var div = document.getElementById(graphics);
-    var height = div.clientHeight;
-    var width = div.clientWidth;
-    var curPos_x, curPos_y, mousePos_x, mousePos_y;
-    var isMouseDown=false, oldScale = 1;
-    var viewBox_x = 0, viewBox_y = 0;
+    const div = document.getElementById(graphics);
+    const height = div.clientHeight;
+    const width = div.clientWidth;
+    let curPos_x, curPos_y, mousePos_x, mousePos_y;
+    let isMouseDown = false;
+    const oldScale = 1;
+    let viewBox_x = 0, viewBox_y = 0;
 
-    var force = d3.layout.force()
+    const force = d3.layout.force()
         .nodes(d3.values(nodes))//set array of nodes
         .links(links)
         .size([width, height])
@@ -158,23 +159,23 @@ function buildGraph(graphics,graphicsid,linkss)
         .start();
 
 
-    var drag = force.drag()
+    const drag = force.drag()
         .on("dragstart", dragstart)
-        .on("dragend",dragend);
+        .on("dragend", dragend);
 
 
-    var rect = document.getElementById("d3-container").getBoundingClientRect()
+    const rect = document.getElementById("d3-container").getBoundingClientRect();
 
-    console.log(rect.width)
-    console.log(rect.height)
-    console.log(this.width)
-    console.log(this.height)
+    console.log(rect.width);
+    console.log(rect.height);
+    console.log(this.width);
+    console.log(this.height);
    //define the
-    var svg = d3.select(graphicsid)
+    const svg = d3.select(graphicsid)
         .append('svg')
         .attr("preserveAspectRatio", "xMidYMid meet")
         //.attr("viewBox",rect.x + " " +rect.x +" "+ rect.width +" "+ 500)
-        .attr("viewBox","130 -250 600 600")
+        .attr("viewBox", "130 -250 600 600");
 
     svg.on("mousedown", function () {
         if (d3.event.defaultPrevented) {
@@ -206,49 +207,57 @@ function buildGraph(graphics,graphicsid,linkss)
     });
 
 
-    var marker=
+    const marker =
         svg.append("marker")
         //.attr("id", function(d) { return d; })
             .attr("id", "resolved")
             //.attr("markerUnits","strokeWidth")
-            .attr("markerUnits","userSpaceOnUse")
+            .attr("markerUnits", "userSpaceOnUse")
             .attr("viewBox", "0 -5 10 10")
-            .attr("refX",38)
+            .attr("refX", 38)
             .attr("refY", -1)
             .attr("markerWidth", 10)
             .attr("markerHeight", 10)
             .attr("orient", "auto")
-            .attr("stroke-width",2)
+            .attr("stroke-width", 2)
             .append("path")
             .attr("d", "M0,-5L10,0L0,5")
-            .attr('fill','#aaa');
+            .attr('fill', '#aaa');
 
     //set link
-    var edges_line = svg.selectAll(".edgepath")
+    const edges_line = svg.selectAll(".edgepath")
         .data(force.links())
         .enter()
         .append("path")
         .attr({
-            'd': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
-            'class':'edgepath',
-            'id':function(d,i) {return 'edgepath'+i;}})
-        .style("stroke",function(d){
+            'd': function (d) {
+                return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y
+            },
+            'class': 'edgepath',
+            'id': function (d, i) {
+                return 'edgepath' + i;
+            }
+        })
+        .style("stroke", function (d) {
             return "#BBB";
         })
         .style("pointer-events", "none")
-        .style("stroke-width",0.5)//storke of lines
-        .attr("marker-end", "url(#resolved)" );//arrow
+        .style("stroke-width", 0.5)//storke of lines
+        .attr("marker-end", "url(#resolved)");//arrow
 
-    var edges_text = svg.append("g").selectAll(".edgelabel")
+    const edges_text = svg.append("g").selectAll(".edgelabel")
         .data(force.links())
         .enter()
         .append("text")
         .style("pointer-events", "none")
         //.attr("class","linetext")
-        .attr({  'class':'edgelabel',
-            'id':function(d,i){return 'edgepath'+i;},
-            'dx':80,
-            'dy':0
+        .attr({
+            'class': 'edgelabel',
+            'id': function (d, i) {
+                return 'edgepath' + i;
+            },
+            'dx': 80,
+            'dy': 0
             //'font-size':10,
             //'fill':'#aaa'
         });
@@ -260,27 +269,27 @@ function buildGraph(graphics,graphicsid,linkss)
         .text(function(d){return d.rela;});
 
     //draw node
-    var circle = svg.append("g").selectAll("circle")
+    const circle = svg.append("g").selectAll("circle")
         .data(force.nodes())
         .enter().append("circle")
-        .style("fill",function(node){
+        .style("fill", function (node) {
             return "#68BDF6";
         })
-        .style('stroke',function(node){
+        .style('stroke', function (node) {
             return "#68AEDD";
         })
         .attr("r", 20)
-        .on("click",function(node){
+        .on("click", function (node) {
 
             sendRequenst(node);
-            edges_line.style("stroke-width",function(line){
+            edges_line.style("stroke-width", function (line) {
 
                 console.log("click it");
                 //todo leave this part waiting for tranfering avaliable
 
-                if(line.source.name==node.name || line.target.name==node.name){
+                if (line.source.name == node.name || line.target.name == node.name) {
                     return 4;
-                }else{
+                } else {
                     return 0.5;
                 }
             });
@@ -293,42 +302,52 @@ function buildGraph(graphics,graphicsid,linkss)
             return
         });
 
-    var text = svg.append("g").selectAll("text")
+    const text = svg.append("g").selectAll("text")
         .data(force.nodes())
         .enter()
         .append("text")
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
-        .style('fill',function(node){
+        .style('fill', function (node) {
             return "#000";
-        }).attr('x',function(d){
+        }).attr('x', function (d) {
             // console.log(d.name+"---"+ d.name.length);
-            var re_en = /[a-zA-Z]+/g;
+            const re_en = /[a-zA-Z]+/g;
             //if english
-            if(d.name.match(re_en)){
+            if (d.name.match(re_en)) {
                 d3.select(this).append('tspan')
-                    .attr('x',0)
-                    .attr('y',2)
-                    .text(function(){return d.name;});
+                    .attr('x', 0)
+                    .attr('y', 2)
+                    .text(function () {
+                        return d.name;
+                    });
             }
             //less than 4
-            else if(d.name.length<=4){
+            else if (d.name.length <= 4) {
                 d3.select(this).append('tspan')
-                    .attr('x',0)
-                    .attr('y',2)
-                    .text(function(){return d.name;});
-            }else{
-                var top=d.name.substring(0,4);
-                var bot=d.name.substring(4,d.name.length);
-                d3.select(this).text(function(){return '';});
+                    .attr('x', 0)
+                    .attr('y', 2)
+                    .text(function () {
+                        return d.name;
+                    });
+            } else {
+                const top = d.name.substring(0, 4);
+                const bot = d.name.substring(4, d.name.length);
+                d3.select(this).text(function () {
+                    return '';
+                });
                 d3.select(this).append('tspan')
-                    .attr('x',0)
-                    .attr('y',-7)
-                    .text(function(){return top;});
+                    .attr('x', 0)
+                    .attr('y', -7)
+                    .text(function () {
+                        return top;
+                    });
                 d3.select(this).append('tspan')
-                    .attr('x',0)
-                    .attr('y',10)
-                    .text(function(){return bot;});
+                    .attr('x', 0)
+                    .attr('y', 10)
+                    .text(function () {
+                        return bot;
+                    });
             }
         });
 
@@ -337,7 +356,7 @@ function buildGraph(graphics,graphicsid,linkss)
         circle.attr("transform", transform1);
         text.attr("transform", transform2);
         edges_line.attr('d', function(d) {
-            var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
+            const path = 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
             return path;
         });
         edges_text.attr('transform',function(d,i){

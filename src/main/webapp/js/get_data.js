8fef84d53281e794f90609e2fa01ed1488227a67
Wenrui Li <u6361099@anu.edu.au>
2019-04-06 16:21:43 +1100

    1

--- src/main/webapp/js/get_data.js
+++ src/main/webapp/js/get_data.js
@@ -1,4 +1,4 @@
-let getJson;
+let getString;
 let nodeArray;
 let baseNodes;
 let baseLinks;
@@ -7,23 +7,27 @@ let links;
 
 
 function updateNode() {
-    console.log("I'm in updateNode()!!!")
-    console.log("getstring: "+getJson);
-    var jsonObjects = JSON.parse(getJson);
-    console.log(jsonObjects);
-    baseNodes = new Array(jsonObjects.length);
-    for (var i = 0; i < jsonObjects.length; i++){
-        console.log(jsonObjects[i]);
+    console.log(getString);
+    nodeArray = getString.split(',');
+
+    console.log(nodeArray);
+    baseNodes = new Array(parseInt(nodeArray.length/4));
+
+    for(let i =0; i<parseInt(nodeArray.length/4);i++){
+        console.log(nodeArray[0]);
         baseNodes[i] = {};
-        baseNodes[i].id = jsonObjects[i].id;
-        baseNodes[i].group = jsonObjects[i].group;
-        baseNodes[i].label = jsonObjects[i].label;
-        baseNodes[i].level = jsonObjects[i].level;
+        baseNodes[i].id = nodeArray[4 *i];
+        baseNodes[i].group = parseInt(nodeArray[4 *i +1]);
+        baseNodes[i].label = nodeArray[4 *i +2];
+        baseNodes[i].level = parseInt(nodeArray[4 *i +3]);
+
     }
 
-    baseLinks = new Array(baseNodes.length/2);
 
-    for(let i =0;i< baseLinks.length;i++){
+
+    baseLinks = new Array(parseInt(baseNodes.length/2));
+
+    for(let i =0;i<baseLinks.length;i++){
         const link = {};
         link.target = baseNodes[i].id;
         link.source = baseNodes[i+1].id;
@@ -50,11 +54,9 @@ function callServer(methodType) {
     xmlResruest.onreadystatechange = function(){
         if(xmlResruest.readyState === 4 && xmlResruest.status === 200){
             document.getElementById("myDiv").innerHTML = "button down";
-            getJson = xmlResruest.responseText;
-            document.getElementById("myDiv").innerHTML = getJson;
-            console.log("I'm before updateNode()!")
+            getString = xmlResruest.responseText;
+            document.getElementById("myDiv").innerHTML = getString;
             updateNode();
-            console.log("I'm after updateNode()!")
             updateSimulation();
         }
 
@@ -62,11 +64,11 @@ function callServer(methodType) {
 
     //let params = "comment=" + "value";
     if(methodType === "GET"){
-        xmlResruest.open("GET","/DaCeMo_war_exploded/Servlet/GraphServlet?"+params,true);
+        xmlResruest.open("GET","/DaCeMo_war_exploded/getGraph?"+params,true);
         xmlResruest.send();
 
     }else if(methodType === "POST"){
-        xmlResruest.open("POST","/DaCeMo_war_exploded/Servlet/GraphServlet",true);
+        xmlResruest.open("POST","/DaCeMo_war_exploded/getGraph",true);
         xmlResruest.setRequestHeader("req","req");
         xmlResruest.send();
 
@@ -256,7 +258,7 @@ function updateGraph() {
     const nodeEnter = nodeElements
         .enter()
         .append('circle')
-        .attr('r', 10*height/630)
+        .attr('r', 10*height/768)
         .attr('fill', function (node) {
             return node.level === 1 ? 'red' : 'gray'
         })
@@ -279,7 +281,7 @@ function updateGraph() {
         .text(function (node) {
             return node.label
         })
-        .attr('font-size', (15*height)/630)
+        .attr('font-size', (15*height)/)
         .attr('dx', 15)
         .attr('dy', 4);
 

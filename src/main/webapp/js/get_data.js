86951d635785826dc29e1c65ce11467f8b06b415
Yu Yang <u6412985@anu.edu.au>
2019-04-20 22:46:58 +1000

    add right click menu

--- src/main/webapp/js/get_data.js
+++ src/main/webapp/js/get_data.js
@@ -417,6 +417,7 @@ function buildGraph(graphics,graphicsid,linkss)
             return "#68AEDD";
         })
         .attr("r", 20)
+        .on('contextmenu', d3.contextMenu(menu))
         .on("click", function (node) {
             console.log(node);
             sendRequenst(node);
@@ -533,3 +534,65 @@ function buildGraph(graphics,graphicsid,linkss)
 
 /*==================================execute the whole script=======================================*/
 
+// Menu Object
+d3.contextMenu = function (menu, openCallback) {
+
+    // create the div element that will hold the context menu
+    d3.selectAll('.d3-context-menu').data([1])
+        .enter()
+        .append('div')
+        .attr('class', 'd3-context-menu');
+
+    // close menu
+    d3.select('body').on('click.d3-context-menu', function() {
+        d3.select('.d3-context-menu').style('display', 'none');
+    });
+
+    // this gets executed when a contextmenu event occurs
+    return function(data, index) {
+        var elm = this;
+
+        d3.selectAll('.d3-context-menu').html('');
+        var list = d3.selectAll('.d3-context-menu').append('ul');
+        list.selectAll('li').data(menu).enter()
+            .append('li')
+            .html(function(d) {
+                return d.title;
+            })
+            .on('click', function(d, i) {
+                d.action(elm, data, index);
+                d3.select('.d3-context-menu').style('display', 'none');
+            });
+
+        // the openCallback allows an action to fire before the menu is displayed
+        // an example usage would be closing a tooltip
+        if (openCallback) openCallback(data, index);
+
+        // display context menu
+        d3.select('.d3-context-menu')
+            .style('left', (d3.event.pageX - 2) + 'px')
+            .style('top', (d3.event.pageY - 2) + 'px')
+            .style('display', 'block');
+
+        d3.event.preventDefault();
+    };
+};
+
+// Define the Menu
+var menu = [
+    {
+        title: 'Item #1',
+        action: function(d, i) {
+            console.log('Item #1 clicked!');
+            console.log('The data for this circle is: ' + d);
+        },
+        disabled: false // optional, defaults to false
+    },
+    {
+        title: 'Item #2',
+        action: function(d, i) {
+            console.log('You have clicked the second item!');
+            console.log('The data for this circle is: ' + d);
+        }
+    }
+]
